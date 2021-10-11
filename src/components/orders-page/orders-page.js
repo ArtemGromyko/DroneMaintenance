cf7303f7ea4be8915b101858e972e61cbb1cc89e
ArtemGromyko <user@DESKTOP-7V56IJ5>
2021-10-11 18:27:16 +0300

    add orders page for admin

--- src/components/orders-page/orders-page.js
+++ src/components/orders-page/orders-page.js
@@ -4,14 +4,61 @@ import { modelContext } from '../../contexts/models-context';
 import { MainContext } from '../../contexts/main-context';
 import { makeStyles } from '@material-ui/core/styles';
 import { getOrders } from '../../services/api-service/orders-service';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+    tableContainer: {
+        width: '70%',
+        margin: '0 auto'
+    }
+});
 
 export default function OrdersPage() {
+    const [rows, setRows] = useState([]);
+    const classes = useStyles();
+    const { user } = useContext(MainContext);
 
     useEffect(() => {
-        getOrders().then((res) => console.log(res));
-    });
+        if(user?.role === 'admin') {
+            getOrders().then((res) => setRows(res));
+        } else {
+            setRows([]);
+        }
+    }, [user]);
 
     return (
-        null
+        <TableContainer className={classes.tableContainer} component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell >id</TableCell>
+            <TableCell align="right">Spare part name</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell >{row.id}</TableCell>
+              <TableCell align="right">{row.sparePartName}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="right">{row.received ? 'received' : 'on the way'}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     );
 }
